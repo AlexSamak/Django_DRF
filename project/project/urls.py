000e425e04990c0c203850f6cb334a4ea804b3d1
AlexSamak <2009kazan@gmail.com>
2020-08-15 15:24:21 +0300

    Начат новый этап

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from orders.views import orders_page, OrderView
+from orders.views import orders_page, OrderView, orders_app
 
 from rest_framework.routers import SimpleRouter
 
@@ -10,6 +10,6 @@ router.register('api/orders', OrderView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', orders_page),
+    path('orders_page/', orders_app),
 ]
-
 urlpatterns += router.urls
